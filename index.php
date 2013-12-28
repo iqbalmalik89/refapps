2288570c9a346889a6284a26a866061ab471dbd7
arooba <arooba.minhas@gmail.com>
2013-12-29 00:44:38 +0500

    login

--- index.php
+++ index.php
@@ -1,115 +1,98 @@
-<!DOCTYPE html>
+
+
 <html>
-<head>
-	<title>fb app</title>
-	<script src="//connect.facebook.net/en_US/all.js"></script>
-    <script src="js/jquery-1.10.js" type="text/javascript" ></script>
-	<script src="js/config.js"></script>    
-</head>
 <body>
-
-
-<div id="fb-root"></div>    
-    
-<script type="text/javascript" >
-
-$(document).ready(function() {
-    
-	FB.init({appId: applicationId, status: true, cookie: true, xfbml: true, oauth: true});
-(function() {
- var e = document.createElement('script');
- e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
- e.async = true;
- document.getElementById('fb-root').appendChild(e);
-}());
-	
-});
-
-
-// FB LOgin 
-function FBlogin() {
-	 
-	   FB.login(function(response) {
-    if (response.status === 'connected') 
-	{
-      var access_token = FB.getAuthResponse()['accessToken']; /*response.authResponse.access_token;*/
-	  FB.api('/me', function(resp) {
-		  		  
-		  var userId = response.authResponse.userID;
-		  if(userId > 0)
-	$.get("https://graph.facebook.com/me?access_token="+access_token, function(data)
-		{	
-			getFriends(access_token,userId);
-								
-		}, "json");
-		  
-		  });
-    }
-	
-  }, {scope: 'user_location, read_friendlists'});
- 
-}
-
-function getFriends(access_token,userId) {
-	var user_friends = new Array();
-    FB.api('/me/friends', function(response) {
-        if(response.data) {
-            $.each(response.data,function(index,friend) {
-			    user_friends[index] = friend.id;
- 				//friends[] = friend.name + ' has id:' + friend.id+'index:'+index;
-            }
-			);
-				console.log(response);
-		
-        } else {
-            alert("Error!");
-        }
+<div id="fb-root"></div>
+<script type="text/javascript" src="js/config.js"></script>
+<script type="text/javascript" src="js/jquery-1.10.js"></script>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : applicationId, // Set YOUR APP ID
+      //channelUrl : 'http://hayageek.com/examples/oauth/facebook/oauth-javascript/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
     });
-}
+ 
+     
+    };
+ 
+    function Login()
+    {
+ 
+        FB.login(function(response) {
+           if (response.authResponse)
+           {
+                getUserInfo();
+            } else
+            {
+             console.log('User cancelled login or did not fully authorize.');
+            }
+         },{scope: 'email,user_photos,user_videos'});
+ 
+    }
+ 
+  function getUserInfo() {
 
-function FBlogout()
-{
-FB.logout(function(response) {
-  // user is now logged out
-  alert('u r logged out!');
-  window.location = "http://vegatechnologies.net/self/index.php";
-});
-}
-
-
+        FB.api('/me/friends', function(response) {
+ 			//console.log(response.data);
+ 			friends = response.data;
+ 			var friendList = "";
+     $.each(response.data,function(index,friend)
+						{
+						   //console.log(friend.name + ' has id:' + friend.id);
+						   friendName	=	friend.name.split(" ");
+						   userImg	=	"http://graph.facebook.com/"+friend.id+"/picture?width=50&height=50";
+						   
+						   friendList	+=	'<h5>'+friend.id+ '</h5>'+friendName[0]+' <span>'+friendName[1]+'</span></h5>';   
+						   
+						   
+					   });
+          
+          //str +="<input type='button' value='Logout' onclick='Logout();'/>";
+          $("#status").append(friendList);
+ 
+    });
+    }
+    function getPhoto()
+    {
+      FB.api('/me/picture?type=normal', function(response) {
+ 
+          var str="<br/><b>Pic</b> : <img src='"+response.data.url+"'/>";
+          document.getElementById("status").innerHTML+=str;
+ 
+    });
+ 
+    }
+    function Logout()
+    {
+        FB.logout(function(){document.location.reload();});
+    }
+ 
+  // Load the SDK asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+ 
 </script>
+<div align="center">
+<h2>Facebook OAuth Javascript Demo</h2>
+ 
+<div id="status">
 
-
-
-<div class="topbar">
-
-
+<input type="button" value="login" onclick="Login()"/>
+</div>
+ 
+<br/><br/><br/><br/><br/>
+ 
+<div id="message">
+</div>
  
 </div>
-
-<div class="section">
-    <div id="demo">	
-     	Demo App
-    </div>
-    
-    <div id="para">
-   		 Facebook login. 
-    </div>
-</div>
-
-<div class="container" style="width:80px;height:50px;margin-top:30px;font-size:15px;">
-
-</div>
-
-<div id="button" >
-	<input type="button" name="btn" class="btn primary large" onClick="FBlogin();" value="Login via Facebook" ></a>
-    <a href="#" class="btn primary " onClick="FBlogout();" >Logout</a>
- </div>
-
-<div class="fb-like" data-send="true" data-width="450" data-show-faces="true" style="margin-top:20px;margin-left:160px;" ></div>
-
-<div id="response" >
-</div>
-
 </body>
 </html>
